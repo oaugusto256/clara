ed5325564f94a61e764623628a2f851f4c5e0b1b
Otavio Augusto <tavioalves@gmail.com>
2026-02-11 10:05:17 -0300

    fix(devops): Make docker container be able to run api on dev envinroment

--- apps/api/src/infra/db/client.ts
+++ apps/api/src/infra/db/client.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as schema from './keywordCategoryMap.schema';
 
-const DATABASE_URL = process.env.DATABASE_URL ?? "postgresql://root:root@db:5432/clara";
+const DATABASE_URL = process.env.DATABASE_URL ?? "postgresql://root:root@localhost:5432/clara";
 const pool = new Pool({
   connectionString: DATABASE_URL,
 });
